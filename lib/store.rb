51245a8963a4bf17e51e4e02858cdb0804f53b6c
Mitchell <meechllada@gmail.com>
2017-09-29 16:38:56 -0700

    add method that capitalizes the first letter of each store entered

--- lib/store.rb
+++ lib/store.rb
@@ -3,4 +3,16 @@ class Store <ActiveRecord::Base
   validates(:name, {:presence => true, :length => { :maximum => 50 }})
   validates :name, uniqueness: true
   validates :name, uniqueness: { case_sensitive: false }
+  before_save(:capitalize_first_letters_name)
+
+  scope(:not_done, ->do
+    where({:done=>false})
+  end)
+
+  private
+
+  def capitalize_first_letters_name
+    name = self.name.split(" ")
+      self.name = name.map{|word| word.capitalize}.join(" ")
+  end
 end
